f596d32e636225306508084b890d9d84656f6a40
Vit2005 <elstan88@gmail.com>
2024-07-18 07:59:13 +0200

    Fixed saving update between sessions

--- Assets/Scripts/Core/GameController.cs
+++ Assets/Scripts/Core/GameController.cs
@@ -56,6 +56,7 @@ public class GameController : MonoBehaviour {
                 {
                     GameObject hitGameObject = hit.collider.gameObject; //banana game object
                     GameEntities.GoldController.AddGold();
+                    GameEntities.SocketConnection.GetGold();
                     Destroy(hitGameObject);
                 }
             }
